f770d3d914e3922d117cd3c51276f9aed0e6c28c
FedericaSpini <spini.1588482@studenti.uniroma1.it>
2019-12-10 12:10:18 +0100

    dynamic doc structure

---
+++ FillInEditor/app/src/main/java/it/uniroma1/fillineditor/models/DynamicDoc.java
@@ -0,0 +1,10 @@
+package it.uniroma1.fillineditor.models;
+
+import java.util.HashMap;
+
+public class DynamicDoc
+{
+    private String name;
+    private String staticText;
+    private HashMap<Integer, DynamicText> dynamicTextHashMap;
+}
---
+++ FillInEditor/app/src/main/java/it/uniroma1/fillineditor/models/DynamicText.java
@@ -0,0 +1,8 @@
+package it.uniroma1.fillineditor.models;
+
+import java.util.ArrayList;
+
+class DynamicText {
+    private int lenght;
+    private ArrayList<Character> characterList;
+}
