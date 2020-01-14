804a04a77827d403ac5166a08224e196762615bb
FedericaSpini <spini.1588482@studenti.uniroma1.it>
2020-01-14 12:30:57 +0100

    character box preview

---
+++ FillInEditor/app/src/main/java/it/uniroma1/fillineditor/viewComponents/StaticCharBox.java
@@ -0,0 +1,24 @@
+package it.uniroma1.fillineditor.viewComponents;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * TODO: document your custom view class.
+ */
+public class StaticCharBox extends View {
+
+
+    public StaticCharBox(Context context) {
+        super(context);
+    }
+
+    public StaticCharBox(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public StaticCharBox(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+}
