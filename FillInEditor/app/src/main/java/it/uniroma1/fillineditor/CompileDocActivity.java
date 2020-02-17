1375bd170cae7c9c86ed8ff2d18b895a0120f265
FedericaSpini <spini.1588482@studenti.uniroma1.it>
2020-02-17 12:28:08 +0100

    The tab bar in docCompilation activity is correctly located

--- FillInEditor/app/src/main/java/it/uniroma1/fillineditor/CompileDocActivity.java
+++ FillInEditor/app/src/main/java/it/uniroma1/fillineditor/CompileDocActivity.java
@@ -12,13 +12,9 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-import it.uniroma1.fillineditor.data.ItemData;
-import it.uniroma1.fillineditor.data.SessionData;
 import it.uniroma1.fillineditor.models.DynamicDocModel;
-import it.uniroma1.fillineditor.viewComponents.WritableCharBoxView;
 
 
-//TODO: Parti dal cambiare l'adapter perché deve gestire in modo differente i due diversi tipi di componenti
 
 public class CompileDocActivity extends AppCompatActivity {
     private TextView mTextMessage;
@@ -32,7 +28,7 @@ public class CompileDocActivity extends AppCompatActivity {
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
         mTextMessage = findViewById(R.id.static_text);
-//        mTextMessage.setText(doc.getTitle());
+        mTextMessage.setText(doc.getTitle());
         navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.doc_recycler);
@@ -50,8 +46,8 @@ public class CompileDocActivity extends AppCompatActivity {
         catch (Exception e){
             System.out.println("This document has not contents!!!!!!!!!!");
         }
-        WritableCharBoxView prova = findViewById(R.id.prova);
-        prova.setItemData(new ItemData(new SessionData(), 4));
+//        WritableCharBoxView prova = findViewById(R.id.prova);
+//        prova.setItemData(new ItemData(new SessionData(), 4));
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
